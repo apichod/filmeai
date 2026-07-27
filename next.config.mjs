191d15229603d8a0c35aaa8cb63d0cfaec0adf1f
Aurelien <aurelien@filme.fr>
2026-07-27 23:17:15 +0300

    fix: googleapis in serverComponentsExternalPackages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {},
-  serverExternalPackages: ['googleapis'],
+  experimental: {
+    serverComponentsExternalPackages: ['googleapis'],
+  },
 }
 export default nextConfig
